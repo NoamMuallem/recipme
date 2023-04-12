bfc5e947eac20b49b07bff19eec5a6780250ee62
Noam Muallem <noammu072@gmail.com>
2023-04-13 00:05:01 +0300

    better layout responsiveness

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,8 +5,10 @@ const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <div>
       <Header />
-      <main className="flex min-h-screen flex-col items-center justify-start bg-gradient-to-b from-[#c9f5d4] to-[#f3f5c9]">
-        {children}
+      <main className="overflow-none flex h-screen justify-center">
+        <div className="flex h-full w-full flex-col border-x border-slate-400 px-2 md:max-w-6xl">
+          {children}
+        </div>
       </main>
     </div>
   );
--- src/components/filters.tsx
+++ src/components/filters.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const Filters = () => {
   return (
-    <div className="my-5 mx-4 flex w-full flex-row justify-evenly bg-accent-content bg-opacity-30 py-2 backdrop-blur-lg">
+    <div className="my-5 flex w-full flex-row justify-evenly rounded-md bg-accent-content bg-opacity-30 py-2 backdrop-blur-lg">
       <div className="btn-group flex flex-row">
         <button className="btn">Top Rated</button>
         <button className="btn">Top Rated</button>
